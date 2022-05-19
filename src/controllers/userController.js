aa8d9f8e0b84fba3fcb94e0cfd04a2632d0b337e
bandall <jsm5315@naver.com>
2022-05-19 20:43:42 +0900

    Before Database

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,7 @@ export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Logout");
+export const search = (req, res) => res.send("Logout");
 export const see = (req, res) =>{ 
     console.log(req.params);
     res.send("See");
